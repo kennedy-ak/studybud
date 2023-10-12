bc152c805fc5e364f98012d79562ee490f5dfc34
kennedy-ak <akogokennedy@gmail.com>
2023-10-12 21:29:29 +0100

    fixing bugs

--- base/views.py
+++ base/views.py
@@ -105,6 +105,9 @@ def createRoom(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             form.save()
             return redirect('home')
     context = {'form':form}
