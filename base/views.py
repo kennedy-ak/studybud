0059deb8e3a236267c73365bad310d9a7c80f27d
kennedy-ak <akogokennedy@gmail.com>
2023-10-12 17:21:46 +0100

    adding crud operation for messaging in a room

--- base/views.py
+++ base/views.py
@@ -129,3 +129,14 @@ def deleteRoom(request,pk):
         return redirect('home')
     return render(request, "base/delete.html",{'obj':room})
 
+def deleteMessage(request,pk):
+    message = Message.objects.get(id = pk)
+
+    if request.user != message.user:
+
+        return HttpResponse("You are not allowed here")    
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, "base/delete.html",{'obj':message})
